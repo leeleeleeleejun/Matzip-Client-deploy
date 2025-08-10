8fac0f7fba0e70d71c20c318e068013d9db99ff8
leeleeleeleejun <wnstjr6293@naver.com>
2025-08-10 20:14:01 +0900

    chore: TypeScript 파일에서 prop-types 룰 비활성화하도록 ESLint 설정 업데이트

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,8 +1,8 @@
-import { nextJsConfig } from '@repo/eslint-config/next-js'
+import { config } from '@repo/eslint-config/react-internal'
 
 /** @type {import("eslint").Linter.Config} */
 export default [
-  ...nextJsConfig,
+  ...config,
   {
     files: ['**/*.{ts,tsx}'],
     rules: {
