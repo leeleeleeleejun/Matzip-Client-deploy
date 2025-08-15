3ebc9f687a2a542032bd96774e12e1cd7e4d192b
leeleeleeleejun <wnstjr6293@naver.com>
2025-08-15 21:14:31 +0900

    feat: Button 컴포넌트에 restProps 추가

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -18,7 +18,13 @@ export type ButtonType = <C extends ElementType = 'button'>(
   props: PropsWithChildren<Props<C>>,
 ) => JSX.Element
 
-export const Button: ButtonType = ({ as, className, size, children }) => {
+export const Button: ButtonType = ({
+  as,
+  className,
+  size,
+  children,
+  restProps,
+}) => {
   const Component = as || 'button'
 
   return (
@@ -33,6 +39,7 @@ export const Button: ButtonType = ({ as, className, size, children }) => {
         BUTTON_SIZE[size],
         className,
       )}
+      {...restProps}
     >
       <Text as={'span'} variant={BUTTON_FONT_SIZE[size]}>
         {children}
