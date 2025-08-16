647083d86ec2b78b9c5a64f8b0513a1362d192fa
leeleeleeleejun <wnstjr6293@naver.com>
2025-08-16 16:06:41 +0900

    feat: Axios 인스턴스 기본 URL 설정 추가

--- apps/web/app/_mocks/handlers/categoryHandlers.ts
+++ apps/web/app/_mocks/handlers/categoryHandlers.ts
@@ -2,8 +2,14 @@ import { http, HttpResponse } from 'msw'
 import { category } from '../data/category'
 import { API_PATH } from '@/_constants/path'
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
+
+const addBaseUrl = (path: string) => {
+  return `${BASE_URL}${path}`
+}
+
 export const CategoryHandlers = [
-  http.get('https://example.com' + API_PATH.CATEGORY, () => {
+  http.get(addBaseUrl(API_PATH.CATEGORY), () => {
     return HttpResponse.json(category)
   }),
 ]
