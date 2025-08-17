0321ce62d33764a0b8ee0ddbba43dc2a0ba74685
leeleeleeleejun <wnstjr6293@naver.com>
2025-08-17 22:09:48 +0900

    feat: 메인 페이지 구성 요소 추가 및 데이터 프리패칭 설정

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,37 @@
-import { Flex } from '@repo/ui/components/Layout/Flex'
+import { CLIENT_PATH } from '@/_constants/path'
+import { useCategoryQueries } from '@/_apis/queries/category'
+import { usePlaceQueries } from '@/_apis/queries/place'
+import { HydrationBoundaryPage } from '@/HydrationBoundaryPage'
+import { Categories } from '@/_components/Categories'
+import { BottomNavigation } from '@/_components/BottomNavigation'
+import { OnlyLeftHeader } from '@repo/ui/components/Header'
+import { SearchBar } from '@repo/ui/components/SearchBar'
+import { Column } from '@repo/ui/components/Layout'
+import { Banner } from '@/_components/Banner'
+import {
+  MostLikedPlaces,
+  MostViewsPlaces,
+} from '@/_components/RankingPlaceList'
+import { Divider } from '@repo/ui/components/Divider'
 
 export default function Page() {
   return (
-    <>
-      <Flex as={'main'}>
-        <div>gkldf</div>
-        <div>gkldf</div>
-        <div>gkldf</div>
-      </Flex>
-    </>
+    <HydrationBoundaryPage
+      prefetch={async (queryClient) => {
+        await queryClient.prefetchQuery(useCategoryQueries.list())
+        await queryClient.prefetchQuery(usePlaceQueries.rankingList('likes'))
+      }}
+    >
+      <OnlyLeftHeader icon={'logo'} name={'맛집'} />
+      <SearchBar href={CLIENT_PATH.PLACE_SEARCH} className={'mx-5 mb-5'} />
+      <Column className={'scrollbar-hide gap-4 overflow-auto'}>
+        <Categories />
+        <Banner contents={[1, 2, 3, 4]} />
+        <MostLikedPlaces />
+        <Divider />
+        <MostViewsPlaces />
+      </Column>
+      <BottomNavigation />
+    </HydrationBoundaryPage>
   )
 }
