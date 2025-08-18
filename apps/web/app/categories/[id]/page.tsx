d1467df43c588f7db35a7212621ebba050d9d75a
leeleeleeleejun <wnstjr6293@naver.com>
2025-08-18 17:52:08 +0900

    feat: prefetchQuery에 usePlaceQueries.byCategory 추가

--- apps/web/app/categories/[id]/page.tsx
+++ apps/web/app/categories/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { HydrationBoundaryPage } from '@/HydrationBoundaryPage'
 import { useCategoryQueries } from '@/_apis/queries/category'
 import { CategoryDetailPage } from '@/categories/[id]/CategoryDetailPage'
+import { usePlaceQueries } from '@/_apis/queries/place'
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params
@@ -9,6 +10,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
     <HydrationBoundaryPage
       prefetch={async (queryClient) => {
         await queryClient.prefetchQuery(useCategoryQueries.list())
+        await queryClient.prefetchQuery(usePlaceQueries.byCategory(id))
       }}
     >
       <CategoryDetailPage initId={id} />
