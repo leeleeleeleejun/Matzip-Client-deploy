56f102750f9864ae4441ee4132ecd803e33c7b28
leeleeleeleejun <wnstjr6293@naver.com>
2025-08-21 18:52:22 +0900

    feat: 캠퍼스 선택 기능을 반영하여 getPlacesByCategory 쿼리 추가수정

--- apps/web/app/categories/[id]/page.tsx
+++ apps/web/app/categories/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { HydrationBoundaryPage } from '@/HydrationBoundaryPage'
 import { useCategoryQueries } from '@/_apis/queries/category'
 import { CategoryDetailPage } from '@/categories/[id]/CategoryDetailPage'
-import { usePlaceQueries } from '@/_apis/queries/place'
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params
@@ -10,7 +9,6 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
     <HydrationBoundaryPage
       prefetch={async (queryClient) => {
         await queryClient.prefetchQuery(useCategoryQueries.list())
-        await queryClient.prefetchQuery(usePlaceQueries.byCategory(id))
       }}
     >
       <CategoryDetailPage initId={id} />
