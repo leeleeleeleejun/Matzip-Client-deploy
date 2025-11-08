6a2c710681201262b45aa2c84d75f02188b5c9c0
leeleeleeleejun <wnstjr6293@naver.com>
2025-11-08 14:48:35 +0900

    feat: 로그인 성공 및 실패 페이지 컴포넌트 추가

---
+++ apps/web/app/login/loading/fail/page.tsx
@@ -0,0 +1,5 @@
+const LoginFailPage = () => (
+  // Todo: 로그인 실패 처리 로직
+  <div></div>
+)
+export default LoginFailPage
---
+++ apps/web/app/login/loading/success/page.tsx
@@ -0,0 +1,6 @@
+const LoginSuccessPage = () => {
+  // Todo: accessToken 요청
+  return <div></div>
+}
+
+export default LoginSuccessPage
