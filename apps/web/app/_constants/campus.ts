49b9ea37aa142eb84619dd2da67d832501e47f13
leeleeleeleejun <wnstjr6293@naver.com>
2025-11-09 13:06:20 +0900

    refactor: 천안 캠퍼스 이름 오타 수정

--- apps/web/app/_constants/campus.ts
+++ apps/web/app/_constants/campus.ts
@@ -2,19 +2,19 @@ import { COLOR_VARIANTS } from '@repo/ui/consts/colorVariant'
 
 export const CAMPUS = {
   SINGWAN: '신관',
-  CHEANAN: '천안',
+  CHEONAN: '천안',
   YESAN: '예산',
 } as const
 
 export const CAMPUS_LOCATION = {
   SINGWAN: { latitude: 36.469483428385914, longitude: 127.14059828594706 },
-  CHEANAN: { latitude: 36.85101236046876, longitude: 127.15093333537096 },
+  CHEONAN: { latitude: 36.85101236046876, longitude: 127.15093333537096 },
   YESAN: { latitude: 36.66990298154316, longitude: 126.85937468290652 },
 } as const
 
 export const CAMPUS_COLOR = {
   SINGWAN: COLOR_VARIANTS.red,
-  CHEANAN: COLOR_VARIANTS.blue,
+  CHEONAN: COLOR_VARIANTS.blue,
   YESAN: COLOR_VARIANTS.yellow,
 } as const
 export type CampusType = keyof typeof CAMPUS
