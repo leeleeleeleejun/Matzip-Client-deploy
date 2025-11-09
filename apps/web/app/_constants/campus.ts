edfb9f4dcd99f778879264441a64170130b9fb30
leeleeleeleejun <wnstjr6293@naver.com>
2025-11-09 12:59:51 +0900

    refactor: 캠퍼스 이름을 대문자로 변경 및 useCampusStore의 초기값 수정

--- apps/web/app/_constants/campus.ts
+++ apps/web/app/_constants/campus.ts
@@ -1,21 +1,21 @@
 import { COLOR_VARIANTS } from '@repo/ui/consts/colorVariant'
 
 export const CAMPUS = {
-  singwan: '신관',
-  cheonan: '천안',
-  yesan: '예산',
+  SINGWAN: '신관',
+  CHEANAN: '천안',
+  YESAN: '예산',
 } as const
 
 export const CAMPUS_LOCATION = {
-  singwan: { latitude: 36.469483428385914, longitude: 127.14059828594706 },
-  cheonan: { latitude: 36.85101236046876, longitude: 127.15093333537096 },
-  yesan: { latitude: 36.66990298154316, longitude: 126.85937468290652 },
+  SINGWAN: { latitude: 36.469483428385914, longitude: 127.14059828594706 },
+  CHEANAN: { latitude: 36.85101236046876, longitude: 127.15093333537096 },
+  YESAN: { latitude: 36.66990298154316, longitude: 126.85937468290652 },
 } as const
 
 export const CAMPUS_COLOR = {
-  singwan: COLOR_VARIANTS.red,
-  cheonan: COLOR_VARIANTS.blue,
-  yesan: COLOR_VARIANTS.yellow,
+  SINGWAN: COLOR_VARIANTS.red,
+  CHEANAN: COLOR_VARIANTS.blue,
+  YESAN: COLOR_VARIANTS.yellow,
 } as const
 export type CampusType = keyof typeof CAMPUS
 export const CAMPUS_LIST = Object.keys(CAMPUS) as CampusType[]
