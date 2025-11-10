6502d1c29c0f8bbb8c726983c4c7782857f64d51
leeleeleeleejun <wnstjr6293@naver.com>
2025-11-10 12:50:26 +0900

    feat: Description 컴포넌트에 tags prop 추가 및 Chip 렌더링 로직 수정

--- apps/admin/src/app/request/[id]/_components/Description/Description.tsx
+++ apps/admin/src/app/request/[id]/_components/Description/Description.tsx
@@ -1,22 +1,25 @@
-import { Column } from '@repo/ui/components/Layout'
+import { Column, Flex } from '@repo/ui/components/Layout'
 import { Chip } from '@repo/ui/components/Chip'
 import { Text } from '@repo/ui/components/Text'
 import { RequestDetail } from '../../_api/types'
 
 type Props = {
   description: RequestDetail['description']
+  tags: RequestDetail['tags']
 }
 
-export const Description = ({ description }: Props) => {
+export const Description = ({ description, tags }: Props) => {
   return (
     <Column className={'gap-1.5'}>
       <Text>소개</Text>
       <Text variant={'body2'} className={'whitespace-pre-wrap'}>
         {description}
       </Text>
-      <div>
-        <Chip icon={'calculator'} label={'가성비 좋은'} />
-      </div>
+      <Flex className={'gap-1'}>
+        {tags.map((tag) => (
+          <Chip key={tag.id} icon={tag.iconKey} label={tag.name} />
+        ))}
+      </Flex>
     </Column>
   )
 }
