564870e757a09f33922d6648e70d3e8728aa489c
leeleeleeleejun <wnstjr6293@naver.com>
2025-11-24 15:24:36 +0900

    feat: 카페 및 식당 검색 결과에 Promise.allSettled 적용 - 일부 요청이 실패 시 부분적인 결과라도 사용자에게 제공하기 위해 사용

--- apps/web/app/places/new/_utils/searchCafeAndRestaurant.ts
+++ apps/web/app/places/new/_utils/searchCafeAndRestaurant.ts
@@ -4,10 +4,14 @@ export const searchCafeAndRestaurant = async (params: {
   query: string
   location: { x: number; y: number }
 }) => {
-  const [cafes, restaurants] = await Promise.all([
+  const [cafesResult, restaurantsResult] = await Promise.allSettled([
     getSearchPlaceByKakao({ ...params, categoryCode: 'cafe' }),
     getSearchPlaceByKakao({ ...params, categoryCode: 'restaurant' }),
   ])
+
+  const cafes = cafesResult.status === 'fulfilled' ? cafesResult.value : []
+  const restaurants =
+    restaurantsResult.status === 'fulfilled' ? restaurantsResult.value : []
 
   // 두 결과를 합쳐서 반환
   return [...cafes, ...restaurants]
