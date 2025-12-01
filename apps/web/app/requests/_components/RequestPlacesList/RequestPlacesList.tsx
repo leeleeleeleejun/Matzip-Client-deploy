0c08f48720e8224fd9e6e214c3a0ad82497aab14
leeleeleeleejun <wnstjr6293@naver.com>
2025-12-01 14:16:05 +0900

    feat: RequestPlacesList 컴포넌트에 EmptyFallback 추가하여 데이터가 없을 때 조건부 렌더링 구현

--- apps/web/app/requests/_components/RequestPlacesList/RequestPlacesList.tsx
+++ apps/web/app/requests/_components/RequestPlacesList/RequestPlacesList.tsx
@@ -2,15 +2,22 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { useRequestQueries } from '@/_apis/queries/request'
 import { PlaceListItem } from '../PlaceListItem'
+import { EmptyFallback } from '@/_components/EmptyFallback'
 
 export const RequestPlacesList = () => {
   const { data } = useSuspenseQuery(useRequestQueries.list())
 
   return (
-    <ul className={'p-5'}>
-      {data.map((place) => (
-        <PlaceListItem key={place.placeId} {...place} />
-      ))}
-    </ul>
+    <EmptyFallback
+      isEmpty={data.length === 0}
+      fallbackTitle={'아직 신청한 내역이 없어요!'}
+      fallbackDescription={'나만 알기 아까운 맛집, 직접 제보해보는 건 어때요?'}
+    >
+      <ul className={'p-5'}>
+        {data.map((place) => (
+          <PlaceListItem key={place.placeId} {...place} />
+        ))}
+      </ul>
+    </EmptyFallback>
   )
 }
