9653c1d95b306083e39f64df14f8af57a6817c99
leeleeleeleejun <wnstjr6293@naver.com>
2026-03-17 20:55:10 +0900

    feat: 비로그인 상태에서 이벤트 정보 미존재 시 로그인 안내 메시지 추가

--- apps/web/app/events/lucky-draw/_components/Pages/GuestView/GuestView.tsx
+++ apps/web/app/events/lucky-draw/_components/Pages/GuestView/GuestView.tsx
@@ -9,12 +9,34 @@ import { EventPrizeCard } from '@/events/lucky-draw/_components/EventPrizeCard'
 import { Column, VerticalScrollArea } from '@repo/ui/components/Layout'
 import { Button } from '@repo/ui/components/Button'
 import { CLIENT_PATH } from '@/_constants/path'
+import { Text } from '@repo/ui/components/Text'
 
 export const GuestView = () => {
   const { data } = useSuspenseQuery(useEventQueries.byPublic())
 
   if (!data) {
-    return <EmptyEventState />
+    return (
+      <Column className={'flex-1 p-5'}>
+        <EmptyEventState />
+        <div className='rounded-lg bg-gray-50 p-3 text-center'>
+          <Text
+            fontSize={'sm'}
+            fontWeight={'semibold'}
+            className={'text-gray-500'}
+          >
+            잠깐! 로그인이 되어있지 않으시네요 👀
+            <br />
+            <Link
+              className='text-blue-500 underline underline-offset-2'
+              href={CLIENT_PATH.LOGIN}
+            >
+              로그인
+            </Link>
+            해야 참여했던 이벤트를 확인할 수 있어요!
+          </Text>
+        </div>
+      </Column>
+    )
   }
 
   const { prize, totalWinnersCount, participantsCount } = data
